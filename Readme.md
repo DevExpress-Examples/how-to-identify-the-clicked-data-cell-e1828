cefcce72efe204811bafdf8b5692a985f0031bad
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Identify the Clicked Data Cell
+
+
+<p>This example shows how to identify which cell has been clicked by an end-user (its owning row and column, value).</p>
+
+<br/>
+
+
---
+++ VB/AgDataGrid_CellClick/MainPage.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace AgDataGrid_CellClick
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+
+		Private Sub grid_CellClick(ByVal sender As Object, ByVal e As DevExpress.AgDataGrid.CellClickEventArgs)
+			Dim message As String = String.Format("RowHandle: {0}; Row Visible Index: {1}; Column: {2}; Value: {3}", New Object(){e.Cell.RowHandle, grid.GetRowVisibleIndex(e.Cell.RowHandle), e.Cell.Column.FieldName, e.Cell.Value})
+			MessageBox.Show(message, "Cell Info", MessageBoxButton.OK)
+		End Sub
+	End Class
+End Namespace
