1daf9b84a1f1990d02e243aa58e277b173b02d34
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_CellClick/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AgDataGrid_CellClick {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+
+        private void grid_CellClick(object sender, DevExpress.AgDataGrid.CellClickEventArgs e) {
+            string message = string.Format("RowHandle: {0}; Row Visible Index: {1}; Column: {2}; Value: {3}",
+                new object[]{e.Cell.RowHandle, grid.GetRowVisibleIndex(e.Cell.RowHandle),
+                    e.Cell.Column.FieldName, e.Cell.Value.CellValue});
+            MessageBox.Show(message, "Cell Info", MessageBoxButton.OK);
+        }
+    }
+}
---
+++ VB/AgDataGrid_CellClick/MainPage.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace AgDataGrid_CellClick
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+
+		Private Sub grid_CellClick(ByVal sender As Object, _
+                               ByVal e As DevExpress.AgDataGrid.CellClickEventArgs)
+			Dim message As String = String.Format( _
+"RowHandle: {0}; Row Visible Index: {1}; Column: {2}; Value: {3}", _
+New Object(){e.Cell.RowHandle, grid.GetRowVisibleIndex(e.Cell.RowHandle), _
+                       e.Cell.Column.FieldName, e.Cell.Value.CellValue})
+	      MessageBox.Show(message, "Cell Info", MessageBoxButton.OK)
+		End Sub
+	End Class
+End Namespace
